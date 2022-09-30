09912706c2a2c0c35b3e0e90426fa8cf996fce52
Stephan Kulla <git.mail@kulla.me>
2022-09-30 23:16:49 +0200

    Add utils.py

--- register-serlo-editor.py
+++ register-serlo-editor.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-import subprocess
-import sys
 import time
 
 import requests
 
-from requests.auth import HTTPBasicAuth
+from utils import error, get_docker_container_id, call_edusharing_api, get_current_lti_tool_ids
 
 TIME_TO_WAIT_FOR_EDUSHARING=180
 
@@ -58,11 +56,6 @@ def register_new_serlo_editor():
 def delete_lti_tool(tool_id):
     call_edusharing_api("/admin/v1/applications/" + tool_id, method="DELETE")
 
-def get_current_lti_tool_ids():
-    reponse = call_edusharing_api("/ltiplatform/v13/tools").json()
-
-    return [tool["appId"] for tool in reponse["tools"]]
-
 def wait_until_edusharing_is_running():
     timestamp_before_loop = time.time()
 
@@ -88,28 +81,13 @@ def is_serlo_running():
         return False
 
 def is_edusharing_running():
-    try:
+    #try:
         return call_edusharing_api("/_about").ok
-    except:
-        return False
-
-def call_edusharing_api(path, method="GET", json=None):
-    url = "http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest" + path
-    additional_args = { "json": json } if json != None else {}
-
-    return requests.request(method, url, auth=HTTPBasicAuth("admin", "admin"),
-                            **additional_args)
+    #except:
+    #    return False
 
 def get_repository_service_id():
     return get_docker_container_id("repository-service")
-
-def get_docker_container_id(name):
-    return subprocess.check_output(f"docker ps -q -f 'name={name}'",
-                                   shell=True).decode().strip()
-
-def error(message):
-    print(f"ERROR: {message}", file=sys.stderr)
-    sys.exit(1)
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,26 @@
+import subprocess
+import sys
+
+import requests
+
+from requests.auth import HTTPBasicAuth
+
+def get_current_lti_tool_ids():
+    reponse = call_edusharing_api("/ltiplatform/v13/tools").json()
+
+    return [tool["appId"] for tool in reponse["tools"]]
+
+def call_edusharing_api(path, method="GET", json=None):
+    url = "http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest" + path
+    additional_args = { "json": json } if json != None else {}
+
+    return requests.request(method, url, auth=HTTPBasicAuth("admin", "admin"),
+                            **additional_args)
+
+def get_docker_container_id(name):
+    return subprocess.check_output(f"docker ps -q -f 'name={name}'",
+                                   shell=True).decode().strip()
+
+def error(message):
+    print(f"ERROR: {message}", file=sys.stderr)
+    sys.exit(1)
