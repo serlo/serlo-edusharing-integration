7a60e5f7141f3b3fe1d1672c4300b05596937551
Stephan Kulla <git.mail@kulla.me>
2022-10-01 14:49:26 +0200

    fix: Fix issue with overriting env variables

--- deploy-all.sh
+++ deploy-all.sh
@@ -13,5 +13,7 @@ info "Wait for edusharing and register serlo editor"
 ./register-serlo-editor.py
 
 info "Update CLIENT ID in serlo editor"
+PLATFORM_CLIENT_ID="$(./get-serlo-editor-lti-tool-id.py)"
+echo "PLATFORM_CLIENT_ID=$PLATFORM_CLIENT_ID" > .env.plattform_id
 docker-compose down
 ./deploy-serlo.sh
--- deploy-serlo.sh
+++ deploy-serlo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PLATFORM_CLIENT_ID=$(./get-serlo-editor-lti-tool-id.py) docker-compose up -d
+docker-compose up -d
