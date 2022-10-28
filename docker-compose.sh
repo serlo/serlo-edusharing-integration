c0279da7420c7f11c47a1e00a98d9ac7bc725c3f
Stephan Kulla <git.mail@kulla.me>
2022-10-28 20:46:54 +0200

    Add possibility to setup only edusharing/serlo

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker-compose -p serlo-edusharing \
-  --project-directory . \
-  -f edusharing/edusharing-common.yml \
+COMPOSE_LIST=""
+
+if [ "$SETUP_PROFILE" != "serlo" ]; then
+  COMPOSE_LIST="$COMPOSE_LIST -f edusharing/edusharing-common.yml \
   -f edusharing/edusharing-remote.yml \
   -f edusharing/repository/repository-common.yml \
   -f edusharing/repository/repository-remote.yml \
@@ -11,6 +12,11 @@ docker-compose -p serlo-edusharing \
   -f edusharing/repository/plugin-mongo/plugin-mongo-common.yml \
   -f edusharing/repository/plugin-mongo/plugin-mongo-remote.yml \
   -f edusharing/services/rendering/rendering-common.yml \
-  -f edusharing/services/rendering/rendering-remote.yml \
-  -f serlo.yml \
-  "$@"
+  -f edusharing/services/rendering/rendering-remote.yml"
+fi
+
+if [ "$SETUP_PROFILE" != "edusharing" ]; then
+  COMPOSE_LIST="$COMPOSE_LIST -f serlo.yml"
+fi
+
+docker-compose -p serlo-edusharing --project-directory . $COMPOSE_LIST "$@"
